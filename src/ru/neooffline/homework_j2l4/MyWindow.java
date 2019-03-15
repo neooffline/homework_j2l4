8a31e29428a4c7195f2b683b05acd1e1b8ea96a8
neooffline <admin@neooffline.ru>
2019-03-15 18:30:29 +0300

    Изменение метода textAppend с использованием функционала JTextArea Добавление автрокрутки чата при появлении нового сообщения

--- src/ru/neooffline/homework_j2l4/MyWindow.java
+++ src/ru/neooffline/homework_j2l4/MyWindow.java
@@ -1,6 +1,7 @@
 package ru.neooffline.homework_j2l4;
 
 import javax.swing.*;
+import javax.swing.text.DefaultCaret;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
@@ -10,13 +11,14 @@ import java.util.Locale;
 import java.util.TimeZone;
 
 public class MyWindow extends JFrame {
-    private StringBuilder stringBuilder = new StringBuilder();
     JPanel chat = new JPanel(new FlowLayout());
     JPanel inputSend = new JPanel(new BorderLayout());
     JTextArea chatMessages = new JTextArea(getHeight() / 50, getWidth() / 50);
     JTextField inputMessage = new JTextField(30);
     JScrollPane scrollPane = new JScrollPane(chatMessages);
     JButton sendButton = new JButton("send");
+    DefaultCaret caret = (DefaultCaret) chatMessages.getCaret();
+
 
     public MyWindow() {
         setWindow();
@@ -27,13 +29,18 @@ public class MyWindow extends JFrame {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setBounds(300, 300, 480, 640);
         setTitle("Чат " + getDateTime("full"));
-        chatMessages.setText("Добро пожаловать в чат!!!");
+        chatMessages.setLineWrap(true);
+        chatMessages.setWrapStyleWord(true);
+        chatMessages.setCaretPosition(chatMessages.getDocument().getLength());
+        scrollPane.setWheelScrollingEnabled(true);
+        caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+        chatMessages.setText("Добро пожаловать в чат!!!\n");
         chat.add(scrollPane);
         inputMessage.addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
                 if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-                    appendText(inputMessage.getText());
+                    appendText(inputMessage.getText(), chatMessages);
                     inputMessage.setText("");
                     inputMessage.requestFocus();
                 }
@@ -41,7 +48,7 @@ public class MyWindow extends JFrame {
         });
         inputSend.add(inputMessage, BorderLayout.CENTER);
         sendButton.addActionListener(e -> {
-            appendText(inputMessage.getText());
+            appendText(inputMessage.getText(), chatMessages);
             inputMessage.setText("");
             inputMessage.requestFocus();
         });
@@ -50,8 +57,8 @@ public class MyWindow extends JFrame {
         add(inputSend, BorderLayout.SOUTH);
     }
 
-    private void appendText(String text) {
-        chatMessages.setText(stringBuilder.append(getDateTime("time") + ": " + text + "\n").toString());
+    private void appendText(String text, JTextArea textArea) {
+        textArea.append("user (" + getDateTime("time") + "):\n" + text + "\n");
     }
 
     private String getDateTime(String type) {
